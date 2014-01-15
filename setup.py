76cd57b789d73175e48369354d636451c28f4cd9
st4lk <myhappydo@gmail.com>
2014-01-15 15:48:03 +0400

    put st4lk into setup authors

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='python-bitcoinrpc',
+setup(name='python-bitcoinrpc-tornado',
       version='0.1',
       description='Enhanced version of python-jsonrpc for use with Bitcoin',
       long_description=open('README').read(),
-      author='Jeff Garzik',
-      author_email='<jgarzik@exmulti.com>',
-      maintainer='Jeff Garzik',
-      maintainer_email='<jgarzik@exmulti.com>',
-      url='http://www.github.com/jgarzik/python-bitcoinrpc',
-      packages=['bitcoinrpc'],
+      author='Jeff Garzik <jgarzik@exmulti.com>, Alexey Evseev <alexevseev@gmail.com>',
+      maintainer='Jeff Garzik <jgarzik@exmulti.com>, Alexey Evseev <alexevseev@gmail.com>',
+      url='http://github.com/st4lk/python-bitcoinrpc-tornado',
+      packages=['bitcoinrpc_async'],
       classifiers=['License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)', 'Operating System :: OS Independent'])
